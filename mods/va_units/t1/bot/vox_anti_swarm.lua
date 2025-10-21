5c5e821e724326a762beb0f7a727f9ddc3fd8a40
SeqSEE <SeqSEE@users.noreply.github.com>
2025-10-21 10:12:21 -0400

    units updates

---
+++ mods/va_units/t1/bot/vox_anti_swarm.lua
@@ -0,0 +1,21 @@
+local animations = {
+    -- Standard animations.
+    stand     = {x = 1.08,   y = 1.08},
+    walk      = {x = 0,  y = 1}
+}
+
+va_units.register_unit("vox_anti_swarm", {
+    mesh = "va_units_vox_anti_swarm.gltf",
+    texture ="va_units_vox_anti_swarm.png",
+    visual_size = { x = 1, y = 1},
+    collisionbox = {-0.7, 0, -0.85, 0.7, 2.1, 0.85},
+    selectionbox = { -0.7, 0.0, -0.85, 0.7, 1.95, 0.85 },
+    stepheight = 0.6,
+    hp_max = 10,
+    nametag = "VASB-1",
+    animations = animations,
+    animation_speed = 1.0,
+    movement_speed = 0.8,
+    spawn_item_description = "VASB-1 Unit Spawn",
+    item_inventory_image = "va_units_blueprint.png",
+})
