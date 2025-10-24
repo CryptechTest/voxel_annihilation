e93cb920c9606605d88c12efdc1e752c84265a65
squidicuzz <squid@cryptech.services>
2025-10-24 11:19:27 -0400

    fix get

--- mods/va_structures/core/api.lua
+++ mods/va_structures/core/api.lua
@@ -3,7 +3,7 @@ function va_structures.get_all_structures()
     local structures = va_structures.get_active_structures()
     local s_ents = {}
     for _,s in pairs(structures) do
-        table.insert(s_ents, s.get_entity())
+        table.insert(s_ents, s:get_entity())
     end
     return s_ents
 end
